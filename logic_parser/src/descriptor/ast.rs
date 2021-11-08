9758a92f5135cd5a92f3d51cbdceab8e0ab6b42d
21aslade <andrewmslade@gmail.com>
2021-11-08 09:02:30 -0700

    This isn't haskell

--- logic_parser/src/descriptor/ast.rs
+++ logic_parser/src/descriptor/ast.rs
@@ -17,10 +17,10 @@ pub struct ConsumerDecl {
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
-pub struct RuleDef<'a, S> {
+pub struct RuleDef<'a> {
     name: &'a str,
     values: Vec<&'a str>,
-    body: RuleBody<'a, S>
+    body: RuleBody<'a>
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
@@ -35,28 +35,28 @@ pub struct Relation {
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
-pub enum RuleBody<'a, S> {
-    County(RuleBodyCounty<'a, S>),
-    Truthy(RuleBodyTruthy<'a, S>),
+pub enum RuleBody<'a> {
+    County(RuleBodyCounty<'a>),
+    Truthy(RuleBodyTruthy<'a>),
     Reference(&'a str, Vec<Value<'a>>)
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
-pub enum RuleBodyCounty<'a, S> {
+pub enum RuleBodyCounty<'a> {
     Constant(Literal<County>),
     Reference(&'a str, Vec<Value<'a>>),
-    LinearComb(Vec<(RuleBodyCounty<'a, S>, u32)>),
-    Count(&'a str, Relation, Value<'a>, Box<RuleBodyTruthy<'a, S>>)
+    LinearComb(Vec<(RuleBodyCounty<'a>, u32)>),
+    Count(&'a str, Relation, Value<'a>, Box<RuleBodyTruthy<'a>>)
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
-pub enum RuleBodyTruthy<'a, S> {
+pub enum RuleBodyTruthy<'a> {
     Constant(Literal<Truthy>),
     Reference(&'a str, Vec<Value<'a>>),
     Access(&'a str, Value<'a>),
-    Compare(Box<RuleBodyCounty<'a, S>>, u32),
-    Exists(&'a str, Relation, Value<'a>, Box<RuleBodyTruthy<'a, S>>),
-    All(Vec<RuleBodyTruthy<'a, S>>),
+    Compare(Box<RuleBodyCounty<'a>>, u32),
+    Exists(&'a str, Relation, Value<'a>, Box<RuleBodyTruthy<'a>>),
+    All(Vec<RuleBodyTruthy<'a>>),
     CheckPosterior(Vec<StateBody>),
     CheckPrior(Vec<StateBody>),
 }
