e9ac319c2cf2bc65380d2b413c5fc1f806758ed5
21aslade <andrewmslade@gmail.com>
2022-02-08 08:40:46 -0700

    Outline descriptor trait
    Co-authored-by: Ibot02 <Ibot02@users.noreply.github.com>
    Co-authored-by: Henny022 <Henny022@users.noreply.github.com>

--- rando_core/src/descriptor.rs
+++ rando_core/src/descriptor.rs
@@ -2,9 +2,33 @@ use std::{hash::Hash, collections::{HashMap, HashSet}};
 
 use crate::{algebra::{Sphery, Truthy, County}, shuffles::Shuffle};
 
-
 pub trait Descriptor<V: Hash> {
-    
+    type Truthy;
+    type County;
+    fn eval<R, T, C>(v: &[V], t : T, c : C) -> R
+    where
+        T : Fn(Self::Truthy) -> R,
+        C : Fn(Self::County) -> R;
+    fn eval_truthy<R, C, Ref, A, Comp, Ex, Conj, Disj, Prio, Post>(body : Self::Truthy, c : C, r : Ref, a : A, comp : Comp, ex : Ex, prio : Prio, post : Post) -> R
+    where
+        C : Fn(Oolean) -> R,
+        Ref : Fn(&str, &[V]) -> R,
+        A : Fn(&str, &[V]) -> R,
+        Comp : Fn(&Self::County, Ntgr) -> R,
+        Ex : Fn(Relation<'_>, &V, &dyn Fn(&V) -> Self::Truthy) -> R,
+        Conj : Fn(&Self::Truthy, &Self::Truthy) -> R,
+        Disj : Fn(&Self::Truthy, &Self::Truthy) -> R,
+        Prio : Fn(&[(Bool, V)]) -> R,
+        Post : Fn(&[(Bool, V)]) -> R;
+    fn eval_county<R, C, Ref, Comb, Min, Max, Ct>(body: Self::County, c: C, r: Ref, comb: Comb, min: Min, max: Max, ct: Ct) -> R
+    where
+        C: Fn(Oolean) -> R,
+        Ref: Fn(&str, &[V]) -> R,
+        A: Fn(&str, &[V]) -> R,
+        Comb: Fn(&Self::County, u32, &Self::County, u32) -> R,
+        Min: Fn(&Self::County, &Self::County) -> R,
+        Max: Fn(&Self::County, &Self::County) -> R,
+        Ct: Fn(Relation<'_>, &V, &dyn Fn(&V) -> Self::County) -> R;
 }
 
 trait Logic<N, V> {}
@@ -16,8 +40,8 @@ where
     L: Logic<LN, V> + Hash,
     LN: Hash,
     V: Hash,
-    T: Truthy + Sphery,
-    C: County<T> + Sphery,
+    T: Truthy + Sphery + Statey,
+    C: County<T> + Sphery + Statey,
     R: Shuffle<V, V>
 {
     type Err;
@@ -25,6 +49,7 @@ where
     fn initialize(
         shuffles: HashMap<&'a str,R>,
         logic : HashSet<L>,
+        descriptors_keysy : HashMap<&'a str, (&'a str, &'a str)>,
         descriptors_truthy : HashMap<&'a str, Vec<D>>,
         descriptors_county : HashMap<&'a str, Vec<D>>
     ) -> Self;
@@ -32,6 +57,7 @@ where
     fn query_descriptor_truthy(&self, name: &str, values : &[V]) -> Result<T, Self::Err>;
     fn query_descriptor_county(&self, name: &str, values : &[V]) -> Result<C, Self::Err>;
     fn query_logic_node(&self, ln : &LN) -> Result<T, Self::Err>;
+    fn query_access(&self, name: &str, values: &[V]) -> Result<T, Self::Err>;
 
     fn mod_shuffle(&mut self, delta : &R::Delta);
 }
--- rando_core/src/lib.rs
+++ rando_core/src/lib.rs
@@ -1,6 +1,3 @@
-use core::fmt;
-use std::fmt::{Display, Formatter};
-
 pub mod descriptor;
 mod algebra;
 mod shuffles;
